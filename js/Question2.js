147d870a771091ca1ac392a6d240cbd4c712eedd
willardwelch <123895302+willardwelch@users.noreply.github.com>
2025-02-09 22:15:59 -0500

    requires all values

--- js/Question2.js
+++ js/Question2.js
@@ -8,6 +8,25 @@ function getUserInfo(){
     let LastName = prompt("Please enter Last Name");
     let age = prompt("Please enter age");
 
+    while(FirstName=="" || LastName=="" || age=="")
+    {
+      if(FirstName=="")
+        {
+        
+          FirstName = prompt("Please enter First Name");
+        }
+       else if(LastName=="")
+          {
+          
+            LastName = prompt("Please enter Last Name");
+          }
+        else if(age=="")
+        {
+          age = prompt("Please enter age");
+        }
+
+
+    }
 
   alert("Name: " + FirstName + " "+LastName+ "," +  " age  "+age);
  
